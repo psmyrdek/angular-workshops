89f31098d508476ad4f23d92fef5f0a7cca8e808
Przemek Smyrdek <przemek.smyrdek@gmail.com>
2018-09-04 19:50:33 +0200

    Filter with 3rd party library

--- src/app/postings-list/postings-list.module.ts
+++ src/app/postings-list/postings-list.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PostingsListComponent } from './postings-list/postings-list.component';
 import { PostingsListEntryComponent } from './postings-list-entry/postings-list-entry.component';
+import { PostingsFiltersModule } from 'ngx-postings-filters';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    PostingsFiltersModule
   ],
   declarations: [PostingsListComponent, PostingsListEntryComponent],
   exports: [PostingsListComponent, PostingsListEntryComponent]
--- src/app/postings-list/postings-list/postings-list.component.ts
+++ src/app/postings-list/postings-list/postings-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { PostingsListService } from '../services/postings-list.service';
 import { Posting } from 'shared/models/Posting.model';
+import { PostingType } from 'shared/enums/PostingType.enum';
 
 @Component({
   selector: 'app-postings-list',
@@ -10,11 +11,29 @@ import { Posting } from 'shared/models/Posting.model';
 export class PostingsListComponent implements OnInit {
 
   postings: Posting[] = [];
+  postingsView: Posting[] = [];
 
   constructor(private service: PostingsListService) { }
 
   ngOnInit() {
     this.postings = this.service.getAll();
+    this.postingsView = [...this.postings];
+  }
+
+  filterByPublic() {
+    this.postingsView = this.postings.filter(x => x.type === PostingType.Public);
+  }
+
+  filterByReallyOld() {
+
+  }
+
+  filterByJobFilledJobDesc() {
+    this.postingsView = this.postings.filter(x => x.jobAd && x.jobAd.jobDescription);
+  }
+
+  resetFilters() {
+    this.postingsView = [...this.postings];
   }
 
 }
